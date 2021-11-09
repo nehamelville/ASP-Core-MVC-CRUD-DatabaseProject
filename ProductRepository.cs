bd86fd49480722defc1a8bcf15d012652fc435f7
nehamelville <neha.melville@gmail.com>
2021-11-08 20:24:09 -0800

    init

---
+++ ProductRepository.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Dapper;
+using Testing.Models;
+
+namespace Testing
+{
+    public class ProductRepository:IProductRepository
+    {
+        private readonly IDbConnection _conn;
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+       
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("Select * from Products;");
+        }
+
+        public Product GetProduct(int id)
+        {
+            return _conn.QuerySingle<Product>("Select * from Products where ProductId=@id", new{id=id}) ;
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _conn.Execute("Update Products set name=@name,price=@price where ProductId=@id",
+                new {name=product.Name,price=product.Price,id=product.ProductID});
+        }
+        public void InsertProduct(Product productToInsert)
+        {
+            _conn.Execute("INSERT INTO products (NAME, PRICE, CATEGORYID) VALUES (@name, @price, @categoryID);",
+                new { name = productToInsert.Name, price = productToInsert.Price, categoryID = productToInsert.CategoryID });
+        }
+        public IEnumerable<Category> GetCategories()
+        {
+            return _conn.Query<Category>("SELECT * FROM categories;");
+        }
+        public Product AssignCategory()
+        {
+            var categoryList = GetCategories();
+            var product = new Product();
+            product.Categories = categoryList;
+
+            return product;
+        }
+        public void DeleteProduct(Product product)
+        {
+            _conn.Execute("DELETE FROM REVIEWS WHERE ProductID = @id;",
+                                       new { id = product.ProductID });
+            _conn.Execute("DELETE FROM Sales WHERE ProductID = @id;",
+                                       new { id = product.ProductID });
+            _conn.Execute("DELETE FROM Products WHERE ProductID = @id;",
+                                       new { id = product.ProductID });
+        }
+
+
+
+    }
+}
